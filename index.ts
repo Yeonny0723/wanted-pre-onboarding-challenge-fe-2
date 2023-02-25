de838558e1f438f8d719cc7bd878b6b61b003eab
Juyeon Kim <kkjuyeon@gmail.com>
2023-02-25 20:47:43 +0900

    Q4

--- index.ts
+++ index.ts
@@ -1,16 +1,12 @@
-/*
-    Q3. 앞 Q2에서 기존 User 타입이 유니온 타입의 Person 타입으로 변경되며 
-        각 타입 별 확실히 존재하지 않는 속성을 꺼내려할 때 에러가 발생함. 
-    A3. (2) person.role과 같이 속성을 바로 꺼내려는 시도 대신, 속성이 객체 내 존재하는("role" in person) 조건을 줄 수 있다. 
-*/
-
 interface User {
+  type: "user";
   name: string;
   age: number;
   occupation: string;
 }
 
 interface Admin {
+  type: "admin";
   name: string;
   age: number;
   role: string;
@@ -20,36 +16,39 @@ export type Person = User | Admin;
 
 export const persons: Person[] = [
   {
+    type: "user",
     name: "Max Mustermann",
     age: 25,
     occupation: "Chimney sweep",
   },
-  {
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
+  { type: "admin", name: "Jane Doe", age: 32, role: "Administrator" },
+  { type: "user", name: "Kate Müller", age: 23, occupation: "Astronaut" },
+  { type: "admin", name: "Bruce Willis", age: 64, role: "World saver" },
 ];
 
+export function isAdmin(person: Person) {
+  return person.type === "admin";
+}
+
+export function isUser(person: Person) {
+  return person.type === "user";
+}
+
 export function logPerson(person: Person) {
-  let additionalInformation: string;
-  if ("role" in person) {
-    // --- (1)
+  let additionalInformation: string = "";
+  if (isAdmin(person)) {
     additionalInformation = person.role;
-  } else {
+  }
+  if (isUser(person)) {
     additionalInformation = person.occupation;
   }
   console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
 }
 
-persons.forEach(logPerson);
+console.log("Admins:");
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log("Users:");
+persons.filter(isUser).forEach(logPerson);
