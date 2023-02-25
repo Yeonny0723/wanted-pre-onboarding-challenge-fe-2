e660246881fec2fc913fd0221bcc195f5fa39a55
Juyeon Kim <kkjuyeon@gmail.com>
2023-02-25 20:39:57 +0900

    A2

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+/*
+    Q. DB에 Admin을 추가해야하는 상황. 
+    기존 User 타입만 있던 DB에 Admin을 어떻게 추가할 수 있을까?
+    A. 기존 User 타입 혹은 새로운 Admin 타입 둘 중 하나를 만족시키는 타임으로 (1) Union Person 타입을 생성한다. 
+       User 타입만 참조하던 (2) 코드에 Admin 타입이 들어올 수 있기에 인풋 타입을 Person으로 변경한다. 
+*/
+
 interface User {
   name: string;
   age: number;
@@ -10,9 +17,9 @@ interface Admin {
   role: string;
 }
 
-export type Person = unknown;
+export type Person = User | Admin; // --- (1)
 
-export const persons: User[] /* <- Person[] */ = [
+export const persons: Person[] = [
   {
     name: "Max Mustermann",
     age: 25,
@@ -35,7 +42,8 @@ export const persons: User[] /* <- Person[] */ = [
   },
 ];
 
-export function logPerson(user: User) {
+export function logPerson(user: Person) {
+  // --- (2)
   console.log(` - ${user.name}, ${user.age}`);
 }
 
